03d5e65bcd8382af5433cb193b037e1bf42a0aa5
Abubakar Hafeez <138915696+AbubakarS989@users.noreply.github.com>
2024-07-20 18:48:00 +0500

    change fil from test py to main py

--- Habit Tracker Day 37/main.py
+++ Habit Tracker Day 37/main.py
@@ -1,124 +1,351 @@
-import requests
 
 
 
-
-# post - upload new data
-# put - update the existing data
-# pull - get the data
-# delete - delete the data
-
-print("------------- Welcome to Habit Tracker -------------------\n")
-USER_NAME=input("Enter your Username : \n")
-TOKEN=input("Enter your Token : \n")
-print("------------- Select one of them -------------------\n")
-print("""
-    1: Add your today Track.
-    2: Update your particular Track.
-    3: Delete your particular Track.
-    4: Show your particular Track Value
-        """)
-    
-
-
-
-
-# ! For Token and Username visit folder 
-# - D:\Endpoints
-# ? Step 1: To create user account 
-# url endpoint
-pixela_endpoint="https://pixe.la/v1/users"
-user_params={    
-"token":TOKEN,
-"username":USER_NAME,
-"agreeTermsOfService":"yes",
-"notMinor":"yes",
-}
-# uncomment, to create a new account 
-# r=requests.post(url=pixela_endpoint,json=user_params)
-# print(r.text)
-# account is created at pixela
-
-
-# ? Step 2: Create new graph 
-graph_id="bkrgraph1"
-graph_params={
-    "id":graph_id,
-    "name": "Code Tracker Graph",
-    "unit":"h",
-    "type":"float",
-    "color":"shibafu" # green
-    
-}
-header={
-    "X-USER-TOKEN":TOKEN
-}
-
-# get entire track:
-pixels_paras={
-            "from":"20240714",
-            "to":"20240719",
-            "withBody":True
-        }
-get_endpoint = f"{pixela_endpoint}/{USER_NAME}/graphs/{graph_id}/pixels"
-r = requests.get(url=get_endpoint,params=pixels_paras, headers=header)
-r.raise_for_status()
-qty=r.json()
-print(qty)
-# endpoint for to check graph
-graph_endpoint=f"{pixela_endpoint}/{USER_NAME}/graphs"
-# r=requests.post(url=graph_endpoint,json=graph_params,headers=header)
-# print(r.text)
-
-# ? Step 3: Upload your data to graph
 from datetime import datetime
-
-current_date=datetime.now().date().strftime("%Y%m%d")
-# print(current_date)
-data_param={
-    "date":current_date,
-    "quantity":input("How many hours did you work today?")
-    # "quantity":"3.4",
-    }
-
-# ? This is for new value
-
-# new data endpoint
-data_endpoint=f"{pixela_endpoint}/{USER_NAME}/graphs/{graph_id}"
-# r=requests.post(url=data_endpoint,json=data_param,headers=header)
-# print(r.text)
-# want to update the value then we use [put method]
-
-# ? This is for to update the existing value
-update_value={
- "quantity": "7.30"  
-}
-# Uncomment , to get the manual data
-manual_date=datetime(year=2024,month=7,day=14).strftime("%Y%m%d")
-
-# new endpoint for updating existing values
-
-update_endpoint=f"{pixela_endpoint}/{USER_NAME}/graphs/{graph_id}/{manual_date}"
-# r=requests.put(url=update_endpoint,json=update_value,headers=header,)
+import requests
+import json
 
 
-# ? This for to get the data from the your graph
+class HABIT_TRACKER_CWA:
+    def __init__(self):
+        # ? Define General Variable used throughout the program.
+        self.USER_NAME = ""
+        self.TOKEN = ""
+        self.graph_id = "bkrgraph1"
+        self.date = ""
+        self.pixela_endpoint = "https://pixe.la/v1/users"
+        self.header = {}
+        self.current_date = datetime.now().strftime("%Y%m%d")
 
-# new endpoint for getting data
-# v1/users/<username>/graphs/<graphID>/<yyyyMMdd> -FORMAT
-get_endpoint=f"{pixela_endpoint}/{USER_NAME}/graphs/{graph_id}/{manual_date}"
+    def Home_Screen(self):
+        '''
+        Display Number of option.
+        Input 1: User Name
+        Input 2: Token
+        Verify the user otherwise program exist 
+        '''
+        # ? Get Login Details, then display further options.
+        print("------------- Welcome to Habit Tracker -------------------\n")
+        self.USER_NAME = input("Enter your Username: \n")
+        self.TOKEN = input("Enter your Token: \n")
+        self.header = {
+            "X-USER-TOKEN": self.TOKEN
+        }
+        
+        #! User Authentication: Check user exist or not 
+        # Encounter the error
+        if self.USER_NAME and self.TOKEN :
+            try:
+                r=requests.get(url=f"https://pixe.la/@{self.USER_NAME}")
+                r.raise_for_status()
+            except requests.exceptions.HTTPError as e:
+                print("-------------     User not found    -------------------",e)
+                
+            if r:
+                print("-------------     User Verified    -------------------\n")
+                
+                print("------------- Select one of following -------------------\n")
+                print("""
+                1: Add your today Track.
+                2: Update your particular Track.
+                3: Delete your particular Track.
+                4: Get your particular Track Value
+                5: Get your Entire Track
+                6: Get your Graph Statistics
+                7:Exit
+                """)
+                choice = int(input("Enter your choice: "))
+                if choice == 1:
+                    self.add_track()
+                elif choice == 2:
+                    self.update_track()
+                    pass
+                elif choice == 3:
+                    self.delete_track()
+                    pass
+                elif choice == 4:
+                    self.track__particular_value()
+                elif choice == 5:
+                    self.get_entire_value()
+                elif choice == 6:
+                    self.graph_statistics()
+                elif choice == 7:
+                    print("Closing the program.....")
+                    exit()
+                else:
+                    print("Invalid choice. Please try again.")
+                
+        else:
+            print("Please Enter your details.")
+            exit()
 
-# r=requests.get(url=get_endpoint,headers=header)
-# print(r.raise_for_status())
-# print(r.text) # {"quantity":"0.30"}
+    def add_track(self):
+        '''Add new track to graph 
+           Take number of hours as an input
+           Input Type :  Float
+        '''
+        
+        quantity = input("How many hours did you work today? ")
+        data_param = {
+            "date": self.current_date,
+            "quantity": quantity
+        }
+        data_endpoint = f"{
+            self.pixela_endpoint}/{self.USER_NAME}/graphs/{self.graph_id}"
+        # data_endpoint=f"{pixela_endpoint}/{USER_NAME}/graphs/{graph_id}"
+        try:
+            r = requests.post(url=data_endpoint,json=data_param, headers=self.header)
+            print("Today Record is added successfully!")    
+        except requests.exceptions.RequestException as e:
+            print("Failed to add track:", e)
 
-#? This for to delete the particular data from the your graph
+    def update_track(self):
+        """
+        Update your desire track.
+        input 1: Day [int]
+        input 2: Month [int]
+        input 3: Your Track value in [float]
+        """
+        print("------------ Update your Track ----------")
+        days = int(input("Enter your desire day to update value [1-31]:"))
+        months = int(input("Enter month of that date [1-12]:"))
+        quantity = input("Enter your update value [hours]:")
 
-# new endpoint for deleting data fro graph
-# the parameters are same as getting data api
-del_endpoint=f"{pixela_endpoint}/{USER_NAME}/graphs/{graph_id}/{manual_date}"
+        # ? Data is updated through this format
+        update_value = {
+            "quantity": quantity
+        }
 
-# r=requests.delete(url=del_endpoint,headers=header)
-# print(r.text) 
+        # Get manual date
+        manual_date = datetime(year=2024, month=months,
+                               day=days).strftime("%Y%m%d")
+
+        # ?  New endpoint for updating existing values
+        update_endpoint = f"{self.pixela_endpoint}/{self.USER_NAME}/graphs/{self.graph_id}/{manual_date}"
+        try:
+            r = requests.put(url=update_endpoint,json=update_value, headers=self.header)
+            print("Your data is updated successfully")
+        except requests.exceptions.RequestException as e:
+            print(f"Failed to update value for {self.date}:", e)
+
+    def delete_track(self):
+        '''
+        Delete Particular Track
+        Input 1: Day [int]
+        Input 2: month [int]
+        It ask [yes/no] before permanently deleting the value
+        '''
+        print("------------ Delete your Track ----------")
+        days = int(input("Enter your day  [1-31]:"))
+        months = int(input("Enter month of that date [1-12]:"))
+
+        # Get manual date
+        manual_date = datetime(year=2024, month=months,day=days).strftime("%Y%m%d")
+
+        # ?Endpoint for deleting the value
+        del_endpoint = f"{self.pixela_endpoint}/{self.USER_NAME}/graphs/{self.graph_id}/{manual_date}"
+
+        # ? Endpoint for getting the required value
+        ''' I used this because, the user verify the data that he want to delete before the program permanently deleted it '''
+        display_endpoint = f"{self.pixela_endpoint}/{self.USER_NAME}/graphs/{self.graph_id}/{manual_date}"
+        try:
+            r = requests.get(url=display_endpoint, headers=self.header)
+            r.raise_for_status()
+            qty = r.json()
+            # print(qty)
+            
+            get_date = datetime.strptime(manual_date, "%Y%m%d").strftime("%d-%m-%Y")
+            
+            # get the user value to display for user authentication
+            print("Are you sure to delete your following track?: ")
+            print(f''' Track Record:
+            Date: {get_date}
+            Quantity: {qty["quantity"]}''')
+            # If user say [yes] the value will deleted permanently
+            ask = input("Enter your choice [yes/no]: ").lower()
+            if ask == "yes":
+                try:
+                    r = requests.delete(url=del_endpoint, headers=self.header)
+                    print("The value has been deleted successfully.")
+                except requests.exceptions.RequestException as e:
+                    print(f"Failed to delete value for {manual_date}:", e)
+            elif ask == "no":
+                print("The request has been canceled.")
+        except requests.exceptions.RequestException as e:
+            print(f"Failed to get value for {manual_date}:", e)
 
 
+    def track__particular_value(self):
+        '''
+        Easily track your day
+        input 1: Day [int]
+        input 2: Month [int]
+        It display the value corresponding to the entered date by user
+        '''
+        print("------------ Show your Track ----------")
+        days = int(input("Enter your day  [1-31]:"))
+        months = int(input("Enter month of that date [1-12]:"))
+        # Get manual date
+        manual_date = datetime(year=2024, month=months,day=days).strftime("%Y%m%d")
+        # Get day
+        manual_day = datetime.strptime(manual_date,"%Y%m%d").strftime("%A")
+
+        # Format the date in decent way for display
+        get_date = datetime.strptime(manual_date, "%Y%m%d").strftime("%d-%m-%Y")
+
+        # ? Endpoint to get value from the graph
+        get_endpoint = f"{self.pixela_endpoint}/{self.USER_NAME}/graphs/{self.graph_id}/{manual_date}"
+        try:
+            r = requests.get(url=get_endpoint, headers=self.header)
+            r.raise_for_status()
+            # print(r.json())
+            # print(qty["quantity"])
+            qty = r.json()
+            print("\nTrack Record:\n")
+            print(f"\tDay: {manual_day}\n\tDate: {get_date}\n\tTime: {qty["quantity"]} Hours\n")
+        except requests.exceptions.RequestException as e:
+            print(f"Failed to retrieve value for {manual_date}:", e)
+
+    def get_entire_value(self):
+        '''
+        Get History, store in json file
+        It display the list of days you posted the value  
+        No inputs
+        '''
+        # Define list to get the particular format that can easily store in json file
+        dic = {}
+        dic1 = {}
+        dic2 = {}
+
+        # ? Fetching existing data from the JSON file if it exists
+        if os.path.exists("Entire Date.json"):
+            try:
+                with open("Entire Date.json", "r") as f:
+                    dic2 = json.load(f)
+            except json.JSONDecodeError:
+                dic2 = {}
+        """
+        stats_endpoint=f"{self.pixela_endpoint}/{self.USER_NAME}/graphs/{self.graph_id}/stats"
+        stats_r=requests.get(url=stats_endpoint,headers=self.header)
+        max_date=stats_r.json()
+        max_date=max_date["maxDate"]
+        lst=[]
+        print(lst.append(str(max_date).split("-")))
+        print(lst)
+        # stats_month=lst[0][1]
+        stats_day=lst[0][2]
+        """
+
+        # change date format to input date
+        """ max_date=datetime.strptime(max_date,"%Y-%m-%d").strftime("%Y%m%d") """
+
+        ''' pixels_paras={
+             "from":max_date,
+             "to":self.current_date,
+             "withBody":True
+         }
+         '''
+        # ? Get the dictionary of dates
+        get_pixels = f"{
+            self.pixela_endpoint}/{self.USER_NAME}/graphs/{self.graph_id}/pixels"
+        try:
+            r = requests.get(url=get_pixels, headers=self.header)
+            r.raise_for_status()
+            qty = r.json()
+
+            # Store dates into list
+            list_of_dates = qty["pixels"]
+
+            for i in list_of_dates:
+                # ? Input each date one by one to get the corresponding value
+                # print(i)
+                manual_date = int(i)
+                # Endpoint for extracting values of desire date
+                get_endpoint = f"{self.pixela_endpoint}/{self.USER_NAME}/graphs/{self.graph_id}/{manual_date}"
+                try:
+                    r = requests.get(url=get_endpoint, headers=self.header)
+                    r.raise_for_status()
+                    format_change = datetime.strptime(i, "%Y%m%d").strftime("%d-%m-%Y")
+                    qty = r.json()
+                    # print(qty)
+
+                    # ? Store value of corresponding date into a dictionary
+                    dic[format_change] = qty["quantity"]
+
+                except requests.exceptions.RequestException as e:
+                    print(f"Failed to retrieve value from {i} to {self.current_date}:", e)
+
+        except requests.exceptions.RequestException as e:
+            print(f"Failed to retrieve value :", e)
+
+        # ? Convert the dictionary into the desired format
+        dic1 = [[date, quantity] for date, quantity in dic.items()]
+        # print(dic1)
+        # check in to protect from error
+        if len(dic) != 0:
+            print("---------------  Your Entire Track Values -----------------\n",)
+            for date, quantity in dic.items():
+                print(f"Date: {date} Time:{quantity} Hour")
+
+            # ? Update data to json file with key date
+            dic2["Your Track Record"] = dic1
+
+            # ? Put the updated data into json file
+            with open("Entire Date.json", "w") as f:
+                json.dump(dic2, f, indent=4)
+
+    def graph_statistics(self):
+        '''
+        Display comprehensive details about your graph
+        1:Total Pixels Count 
+        2:Max Quantity
+        3:Min Quantity
+        4:Total Quantity
+        5:Avg Quantity
+        6:Todays Quantity
+        7:Yesterday Quantity
+        8:Max Date
+        9:Mix Date
+        '''
+        
+        # /v1/users/<username>/graphs/<graphID>/stats
+        stats_endpoint = f"{self.pixela_endpoint}/{self.USER_NAME}/graphs/{self.graph_id}/stats"
+        stats_r = requests.get(url=stats_endpoint, headers=self.header)
+        graph_stats = stats_r.json()
+        #? Example Output from APi
+        # totalPixelsCount":4,"maxQuantity":5,"minQuantity":-5,"maxDate":"2017-12-31","minDate":"2018-01-01","totalQuantity":5,"avgQuantity":1.25,"todaysQuantity":3,"yesterdayQuantity":2}
+        
+        #? store each value as separated VAr to get the desire formate
+        total_pixel=graph_stats["totalPixelsCount"]
+        max_quantity=graph_stats["maxQuantity"]
+        min_quantity=graph_stats["minQuantity"]
+        total_quantity=graph_stats["totalQuantity"]
+        avg_quantity=graph_stats["avgQuantity"]
+        today_qty=graph_stats["todaysQuantity"]
+        yesterday_qty=graph_stats["yesterdayQuantity"]
+        # MAX 
+        max_date=graph_stats["maxDate"]
+        max_date=datetime.strptime(max_date,"%Y-%m-%d").strftime("%d-%m-%Y")
+        max_day=datetime.strptime(max_date,"%d-%m-%Y").strftime("%A")
+        # MIN
+        min_date=graph_stats["minDate"]
+        min_date=datetime.strptime(min_date,"%Y-%m-%d").strftime("%d-%m-%Y")
+        min_day=datetime.strptime(min_date,"%d-%m-%Y").strftime("%A")
+        # Output in decent Format 
+        print("---------------  Graph Statistics -----------------\n")
+        print(f"Total Days you posted  : {total_pixel} Days")
+        print(f"Maximum Hours you work : {max_quantity} hours")
+        print(f"Minimum Hours you work : {min_quantity} hours")
+        print(f"Total Working Hours    : {total_quantity} hours")
+        print(f"Your Average Hours     : {avg_quantity} hours")
+        print(f"Today Work hours       : {today_qty} hours")
+        print(f"Yesterday Work hours   : {yesterday_qty} hours")
+        print(f"Date on which you work less:{min_day} {min_date} ")
+        print(f"Date on which you work more: {max_day} {max_date}")
+        
+        
+
+
+#? Create instance from the Class
+screen = HABIT_TRACKER_CWA()
+screen.Home_Screen()
