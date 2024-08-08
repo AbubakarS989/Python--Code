77e9bf0b3797538eb3ebf394d34a73e46aade249
Abubakar Hafeez <138915696+AbubakarS989@users.noreply.github.com>
2024-08-09 00:10:00 +0500

    learning about using css selector to get data

--- Web Sacraping/main.py
+++ Web Sacraping/main.py
@@ -24,7 +24,7 @@ with open("index.html","r") as f:
 
 soup=BeautifulSoup(html_data,"html.parser")
 
-# print(soup.prettify())
+print(soup.prettify())
 
 
 
@@ -62,9 +62,16 @@ print(soup.h3.string)
 
 # extract all text from a page
 
-# print(soup.get_text())
+print(soup.get_text())
+
+
+
+
+
+
 
 # ? extract all specified tags using tag name
+print("---------------------------------------------------")
 
 all_anchor_tags=soup.find_all(name="a")  #return list 
 # print(all_anchor_tags)
@@ -74,22 +81,55 @@ for link in soup.find_all("a"):
 for link in soup.find_all("a"):
     print(link.get("href")) #get the actual link
     
-    
+
+
+
+
+print("---------------------------------------------------")
     
 # ? target on a specific tag of their specific attribute
 # with id
-specific_id_attribute=soup.find(name="h1",id="target_class")  
+specific_id_attribute=soup.find(name="h1",id="target_class",)  
 print(specific_id_attribute)
 
 
 
+
+
+
+
+print("---------------------------------------------------")
+
 # ? target on a specific tag of their specific attribute
 # with class
 specific_class_attribute=soup.find(name="p",class_="story")  
-print(specific_clas_attribute)
+print(specific_class_attribute)
 
 
 
+
+
+
+
+print("---------------------------------------------------")
+# ! CSS selectors type
+# ? what if we have to select the specific tag from the thousand of tags list
+# we use selector just like in CSS
+#? <h3>I will learn in this module: <a href="https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-beautiful-soup" class="sister" id="link1">Elsie</a></h3>
+
+# i have to dig dive from h3 to a tag
+selector_value=soup.select_one(selector="h3 a") #ex:1 return one item 
+print(selector_value.get("href"))
+
+selector_value=soup.select_one(selector="#target_class") #ex:2 return one item 
+print(selector_value)
+print(selector_value.string)
+
+selector_value=soup.select_one(selector="ol li") #ex:3 return one item 
+print(selector_value)
+print("---------------------------------------------------")
+class_value=soup.select(".story") # return list of all elements
+print(class_value)
 
 
 
